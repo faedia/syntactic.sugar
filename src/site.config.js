520ede4e1fc44e1f8c6d18c160f24a2425f76139
Emma Tomlinson <t97emma@gmail.com>
2025-06-17 21:18:28 +0100

    Update theme and layout

--- src/site.config.js
+++ src/site.config.js
@@ -1,6 +1,6 @@
 export const config = {
     title: 'Syntactic.Sugar',
-    description: 'A personal blog about compiler design, programming languages, and sometimes my life.',
+    description: 'A personal blog about compiler design, programming languages, focusing on a language called LoxMocha being implemented in C++. Often includes discussions on programming language theory, compiler design, and practical coding examples.',
     author: {
         name: 'Emma Tomlinson',
         email: 'emma@syntacticsugar.dev'
